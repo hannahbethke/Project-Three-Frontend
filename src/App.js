fe651d0dd55a8d1910fcd2aff9d7f909fb55f04f
hannahbethke <hannahnichole@gmail.com>
2022-08-30 19:53:23 -0600

    wip: adds pages and links to pages with routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+import Music from './pages/Music';
+import Contact from './pages/Contact';
 import './index.css';
 import './App.css';
 
@@ -5,6 +12,13 @@ function App() {
   return (
     <div className="App">
      <h1>App</h1>
+     <Nav />
+     <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/music" element={<Music />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <Link to="/">
+                <div>ARTIST NAME</div>
+            </Link>
+            <Link to="/about">
+                <div>About</div>
+            </Link>
+            <Link to="/music">
+                <div>Music</div>
+            </Link>
+            <Link to="/contact">
+                <div>Contact</div>
+            </Link>
+        </div>
+    );
+}
+
+export default Nav;
