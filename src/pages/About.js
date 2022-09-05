a14615e34e8119e1d91ad54cf00926877807bedf
hannahbethke <hannahnichole@gmail.com>
2022-09-05 13:56:03 -0600

    changes image on page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,10 @@ const About = () => {
     return (
         <div className="aboutPageContainer">
             <div className="aboutImgDiv">
-                <img className="aboutImg" src={require("../images/headshot3.jpg")} alt="headshot" />
+                <img className="aboutImg" src={require("../images/piano.jpg")} alt="headshot" />
             </div>
             <div className="aboutPageBio">
+            
                 <p>Noah B... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
