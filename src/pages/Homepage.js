131c975410b59713567d2019fa38f37cfcc1b3af
hannahbethke <hannahnichole@gmail.com>
2022-09-05 13:53:15 -0600

    wip: changes images on page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,21 +2,20 @@ import React from 'react';
 
 const Homepage = () => {
     return (
-        <>
-            <div className="homepageContainer">
+        <div className="homepageContainer">
 
-                <div className="homepageLeft">
-                    <img className="homepageImg" src={require("../images/headshot1.jpg")} alt="headshot" />
-                </div>
-
-                <div className="homepageRight"></div>
-
-                <div className="overlay">
-                    NO<span className="homepageSpan">AH</span>
-                </div>
-
+            <div className="homepageLeft">
+                <img className="homepageImg" src={require("../images/pexels-merlin-lightpainting-11308623.jpg")} alt="headshot" />
             </div>
-        </>
+
+            <div className="homepageRight">
+            </div>
+
+            <div className="overlay">
+                NO<span className="homepageSpan">AH</span>
+            </div>
+
+        </div>
     );
 }
 
