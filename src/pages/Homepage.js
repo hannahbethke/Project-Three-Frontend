dde1da57c2f0dedae08ece210026845bae31c7a2
hannahbethke <hannahnichole@gmail.com>
2022-09-09 13:13:49 -0600

    cleans up homepage code and css

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,24 +1,19 @@
-import homepageImg from '../assets/images/pexels-merlin-lightpainting2.jpg'
+import homepageImg from '../assets/images/pexels-merlin-lightpainting2.jpg';
 
 const Homepage = () => {
     return (
         <div className="homepageContainer">
 
-            <div className="homepageLeft">
-                
-            </div>
-
             <div className="homepageRight">
-            <img className="homepageImg" src={homepageImg} alt="face with rainbow color" />
-            {/* <img className="homepageImg" src={require("../images/pexels-merlin-lightpainting2.jpg")} alt="headshot" /> */}
+                <img className="homepageImg" src={homepageImg} alt="face with rainbow color" />
             </div>
 
-            <div className="overlay">
-                <h1 className="overlayH1">Merlin</h1>
+            <div className="overlap">
+                <h1 className="overlapH1">Merlin</h1>
             </div>
 
         </div>
     );
-}
+};
 
 export default Homepage;
