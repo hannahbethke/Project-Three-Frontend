170e863b7a50703dac19188fb596639026e7f89b
hannahbethke <hannahnichole@gmail.com>
2022-09-12 18:56:52 -0600

    cleans up comtact page code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import contactPageImg from '../assets/images/pexels-merlin-lightpainting3.jpg'
+import contactPageImg from '../assets/images/pexels-merlin-lightpainting3.jpg';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import IconButton from '@mui/material/IconButton';
 import TwitterIcon from '@mui/icons-material/Twitter';
@@ -8,41 +8,43 @@ const Contact = () => {
 
     const handleClickInsta = () => {
         window.open("https://www.instagram.com/merlin.lightpainting/");
-    }
+    };
 
     const handleClickTwitter = () => {
         window.open("https://twitter.com/MERLINph");
-    }
+    };
 
     const handleClickWeb = () => {
         window.open("https://www.pexels.com/@merlin/");
-    }
+    };
 
     return (
+
         <div className="contactPage">
             <img className="contactPageImg" src={contactPageImg} alt="Portrait" />
             <h1>Merlin Lightpainting</h1>
             <h3>Photographer, Lightpainter, NFT Artist</h3>
             <h4>Follow to Support!</h4>
             <ul className="contactButtons">
-            <li>
-                <IconButton className="iconButton" onClick={handleClickInsta}>
-                    <InstagramIcon/>
-                </IconButton>   
-            </li>
-            <li>
-                <IconButton className="iconButton" onClick={handleClickTwitter}>
-                    <TwitterIcon/>
-                </IconButton>  
-            </li>
-            <li>
-            <IconButton className="iconButton" onClick={handleClickWeb}>
-                    <LanguageIcon/>
-                </IconButton> 
-            </li>
+                <li>
+                    <IconButton className="iconButton" onClick={handleClickInsta}>
+                        <InstagramIcon/>
+                    </IconButton>   
+                </li>
+                <li>
+                    <IconButton className="iconButton" onClick={handleClickTwitter}>
+                        <TwitterIcon/>
+                    </IconButton>  
+                </li>
+                <li>
+                <IconButton className="iconButton" onClick={handleClickWeb}>
+                        <LanguageIcon/>
+                    </IconButton> 
+                </li>
             </ul>
         </div>
+
     );
-}
+};
 
 export default Contact;
